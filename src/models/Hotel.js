337aeb5f411509291561e8fc705a1c51831a251a
ManuelLiberal <liberalmanuel.em.h2@gmail.com>
2023-05-08 17:52:09 -0300

    Cree los 3 primeros modelos

---
+++ src/models/Hotel.js
@@ -0,0 +1,72 @@
+const { DataTypes, INTEGER } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "Hotel",
+    {
+      id: {
+        type: INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      phoneNumber: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isPhoneNumber: function (value) {
+            if (!/^[0-9()-\s]+$/.test(value)) {
+              throw new Error(
+                "El número de teléfono contiene caracteres no válidos"
+              );
+            }
+          },
+        },
+      },
+      province: {
+        type: DataTypes.ENUM(
+          "BUENOS AIRES",
+          "CATAMARCA",
+          "CHACO",
+          "CHUBUT",
+          "CORDOBA",
+          "CORRIENTES",
+          "ENTRE RIOS",
+          "FORMOSA",
+          "JUJUY",
+          "LA PAMPA",
+          "LA RIOJA",
+          "MENDOZA",
+          "MISIONES",
+          "NEUQUEN",
+          "RIO NEGRO",
+          "SALTA",
+          "SAN JUAN",
+          "SAN LUIS",
+          "SANTA CRUZ",
+          "SANTA FE",
+          "SANTIAGO DEL ESTERO",
+          "TIERRA DEL FUEGO",
+          "TUCUMAN"
+        ),
+        allowNull: false,
+      },
+      location: DataTypes.GEOGRAPHY,
+    },
+    {
+      timestamps: false,
+    }
+  );
+};
