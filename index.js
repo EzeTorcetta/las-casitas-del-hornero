0f081dddaed0fac08acedac08de35be8071ab1e0
Jose Pergasere <111614623+JosePergasere@users.noreply.github.com>
2023-05-08 15:20:04 -0300

    Creacion de Express-Estructura Base

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 //*Se importa el servidor, la base de datos y se conectan ambos
+
+const server = require("./src/app");
+// const { conn } = require("./src/db.js");
+const PORT = process.env.PORT || 3001;
+
+// conn.sync({ force: true }).then(() => {
+//   server.listen(PORT, () => {
+//     console.log(`Server raised in port ${PORT}`); // eslint-disable-line no-console
+//   });
+// });
---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+const { Router } = require("express");
+const router = Router();
+
+//* RUTAS *//
+
+//router.use("/ej")
+//router.use("/ej")
+//router.use("/ej")
+
+//****************************************/
+module.exports = router;
