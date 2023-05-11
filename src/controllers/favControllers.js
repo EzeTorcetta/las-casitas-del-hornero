a7173631e585eebf71fe26d01b189c392c6e4089
ManuelLiberal <liberalmanuel.em.h2@gmail.com>
2023-05-11 18:29:36 -0300

    Termine el Fav controller y modifique un par de nombres en el handler

--- src/controllers/favControllers.js
+++ src/controllers/favControllers.js
@@ -1,17 +1,34 @@
 //?----------------------------IMPORTS--------------------------------
 
-const { Fav } = require("../db");
+const { User, Hotel } = require("../db");
 
 //?----------------------------CONTROLLERS------------------------------
 
 //*------------GET ALL FAVS-------------------
-const getFavs = async () => {};
-
+const getFavs = async (id_user) => {
+  const user = await User.findByPk(id_user);
+  const favs = await user.getHotels();
+  return favs;
+};
 //*------------ADD FAV-------------------
-const postFav = async (fav) => {};
+
+const postFav = async (id_user, id_hotel) => {
+  const user = await User.findByPk(id_user);
+  await user.addHotel(id_hotel);
+  return;
+};
 
 //*------------DELETE FAV-------------------
-const deleteFav = async (fav) => {};
+
+const deleteFav = async (id_user, id_hotel) => {
+  const user = await User.findByPk(id_user);
+  await user.removeHotel({
+    where: {
+      id: id_hotel,
+    },
+  });
+  return;
+};
 
 module.exports = {
   getFavs,
