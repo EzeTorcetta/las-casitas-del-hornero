8be9b8fa03f1877520f6e95e18d920e77447cea7
ezequiel torcetta <119866815+EzeTorcetta@users.noreply.github.com>
2023-05-12 21:10:29 -0300

    Update userRouter.js

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,11 +1,47 @@
-//! A MODIFICAR
-
 const { Router } = require("express");
 const userRouter = Router();
 
 const { getUserHandler, postUserHandler } = require("../handlers/userHandler");
 
-userRouter.get("/", getUserHandler); //enviamos por body el username y pass
-userRouter.post("/", postUserHandler);
+const checkUserProperties = (req, res, next) => {
+  const { email, password, username, admin } = req.body;
 
-module.exports = userRouter;
+  if (email && password && username && typeof admin === "boolean") {
+    return postUserHandler(req, res, next);
+  }
+
+  if (email && password && (typeof admin !== "boolean" || !username)) {
+    return res.status(400).json({ error: "Falta información en la solicitud" });
+  }
+
+  if (email && password && !admin && !username) {
+    return res.status(400).json({ error: "Falta información en la solicitud" });
+  }
+
+  if (email && password && typeof admin === "boolean") {
+    return postUserHandler(req, res, next);
+  }
+
+  if (email && password) {
+    return getUserHandler(req, res, next);
+  }
+
+  return res.status(400).json({ error: "Falta información en la solicitud" });
+};
+
+userRouter.post("/", checkUserProperties);
+
+module.exports = userRouter;
+
+
+
+
+// const { Router } = require("express");
+// const userRouter = Router();
+
+// const { getUserHandler, postUserHandler } = require("../handlers/userHandler");
+
+// userRouter.get("/", getUserHandler); //enviamos por body el username y pass
+// userRouter.post("/", postUserHandler);
+
+// module.exports = userRouter;
