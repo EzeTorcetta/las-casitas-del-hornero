51ef154a41473de0ec80cf8d0e8847d5a32390f6
Ignacio Martin <119757609+maximustxt@users.noreply.github.com>
2023-05-24 16:14:12 -0300

    Cree otra ruta de nodeMailer

--- src/routes/NodeMailerRouter.js
+++ src/routes/NodeMailerRouter.js
@@ -4,9 +4,11 @@ const NodeMailerRouter = Router();
 const {
   getNodeMailerHandler,
   getRegistroNodeMailerHandler,
+  getNodeMailerReservaHandler,
 } = require("../handlers/getNodeMailerHandler");
 
 NodeMailerRouter.get("/", getNodeMailerHandler);
+NodeMailerRouter.get("/Reserva/:gmail", getNodeMailerReservaHandler);
 NodeMailerRouter.get("/Registro/:gmail", getRegistroNodeMailerHandler);
 
 module.exports = NodeMailerRouter;
