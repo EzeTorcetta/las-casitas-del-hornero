1fb6dc07ef9ab251140d434e6bdf5d605bd5b80f
Ignacio Martin <119757609+maximustxt@users.noreply.github.com>
2023-05-24 16:35:20 -0300

    Ruta de baneo

--- src/routes/NodeMailerRouter.js
+++ src/routes/NodeMailerRouter.js
@@ -5,10 +5,12 @@ const {
   getNodeMailerHandler,
   getRegistroNodeMailerHandler,
   getNodeMailerReservaHandler,
+  getNodeMailerBaneoHandler,
 } = require("../handlers/getNodeMailerHandler");
 
 NodeMailerRouter.get("/", getNodeMailerHandler);
-NodeMailerRouter.get("/Reserva/:gmail", getNodeMailerReservaHandler);
 NodeMailerRouter.get("/Registro/:gmail", getRegistroNodeMailerHandler);
+NodeMailerRouter.get("/Reserva/:gmail", getNodeMailerReservaHandler);
+NodeMailerRouter.get("/Baneo/:gmail", getNodeMailerBaneoHandler);
 
 module.exports = NodeMailerRouter;
