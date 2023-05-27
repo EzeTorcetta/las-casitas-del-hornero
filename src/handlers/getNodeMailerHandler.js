feb2bfc23cbc4400db05bc66e448229b633af974
Ignacio Martin <119757609+maximustxt@users.noreply.github.com>
2023-05-27 18:20:52 -0300

    cree ruta de notificacion para cuando se crea un hotel

--- src/handlers/getNodeMailerHandler.js
+++ src/handlers/getNodeMailerHandler.js
@@ -253,9 +253,73 @@ const getNodeMailerBaneoHandler = async (req, res) => {
   }
 };
 
+//?-------------------------------------------Notificacion De Creacion del Hotel:
+
+const getNodeMailerCreacionDeHotelHandler = async (req, res) => {
+  try {
+    const { gmail } = req.params;
+    const { name } = req.query;
+
+    console.log(gmail, name);
+
+    await transporter.sendMail({
+      from: '"CasitasDelHornero " <lacasitadelhornero2023@gmail.com>', // el que va enviar el correo
+      to: gmail, // para quien va a ir el corrreo electronico.
+      //   subject: "Hello ✔", // Subject line
+      text: "Hello world?", // texto plano
+      html: `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <style></style>
+        </head>
+        <body>
+          <div
+            style="
+              width: 100%;
+              background-color: #fd611a;
+              text-align: center;
+              border-radius: 10px;
+            "
+          >
+            <div style="padding: 20px 10px 20px 10px">
+              <img
+                style="width: 100px; height: 100px; border-radius: 50%"
+                src="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/utbvsuv2bhb7gbubbaqk"
+              />
+              <div
+                style="
+                  width: 100%;
+                  text-align: center;
+                  display: flex;
+                  justify-content: center;
+                "
+              >
+                <div style="width: 100%; background-color: white; margin: 20px">
+                <h1>Hola.${name}</h1>
+                <h3>Felicitaciones por haber publicado un Hotel en nuestra web!</h3>
+                <p>Este correo es para confirmar que haz publicado un Hotel.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </body>
+      </html>
+      `,
+    }); // css por dentro del html
+    res.status(200).send("Email Enviado Con exito");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
 module.exports = {
   getNodeMailerHandler,
   getRegistroNodeMailerHandler,
   getNodeMailerReservaHandler,
   getNodeMailerBaneoHandler,
+  getNodeMailerCreacionDeHotelHandler,
 };
