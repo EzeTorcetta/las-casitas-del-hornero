92c7b6d7f42ac6f53984c2e6972f46e0465ca601
ezequiel torcetta <ezequiel.torcetta.97@gmail.com>
2023-05-31 10:48:30 -0300

    Update Room.js

--- src/models/Room.js
+++ src/models/Room.js
@@ -15,7 +15,6 @@ module.exports = (sequelize) => {
       },
       dates: {
         type: ARRAY(DATE),
-        defaultValue: ["2023-05-24", "2023-05-25", "2023-05-26", "2023-05-29", "2023-05-30", "2023-05-31"]
       }
     },
     {
