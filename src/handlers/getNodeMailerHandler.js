55fd1d80cdbded3a369b0f271ab4711bc2d84def
Jose Pergasere <111614623+JosePergasere@users.noreply.github.com>
2023-05-31 19:47:19 -0300

    mail uwu

--- src/handlers/getNodeMailerHandler.js
+++ src/handlers/getNodeMailerHandler.js
@@ -238,7 +238,7 @@ const getNodeMailerBaneoHandler = async (req, res) => {
                 "
               >
                 <div style="width: 100%; background-color: white; margin: 20px">
-                <h1>Tu cuenta a sido baneada temporalmente</h1>
+                <h1>Tu cuenta ha sido bloqueada temporalmente</h1>
                 </div>
               </div>
             </div>
@@ -316,10 +316,68 @@ const getNodeMailerCreacionDeHotelHandler = async (req, res) => {
   }
 };
 
+const getNodeMailerDesbaneoHandler = async (req, res) => {
+  try {
+    const { gmail } = req.params;
+
+    await transporter.sendMail({
+      from: '"CasitasDelHornero " <lacasitadelhornero2023@gmail.com>', // el que va enviar el correo
+      to: gmail, // para quien va a ir el corrreo electronico.
+      //   subject: "Hello ✔", // Subject line
+      text: "Hello world?", // texto plano
+      html: `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <style></style>
+        </head>
+        <body>
+          <div
+            style="
+              width: 100%;
+              background-color: #fd611a;
+              text-align: center;
+              border-radius: 10px;
+            "
+          >
+            <div style="padding: 20px 10px 20px 10px">
+              <img
+                style="width: 100px; height: 100px; border-radius: 50%"
+                src="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/utbvsuv2bhb7gbubbaqk"
+              />
+              <div
+                style="
+                  width: 100%;
+                  text-align: center;
+                  display: flex;
+                  justify-content: center;
+                "
+              >
+                <div style="width: 100%; background-color: white; margin: 20px">
+                <h1>Tu cuenta ha sido desbloqueada</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        </body>
+      </html>
+      `,
+    }); // css por dentro del html
+    res.status(200).send("Email Enviado Con exito");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+
 module.exports = {
   getNodeMailerHandler,
   getRegistroNodeMailerHandler,
   getNodeMailerReservaHandler,
   getNodeMailerBaneoHandler,
   getNodeMailerCreacionDeHotelHandler,
+  getNodeMailerDesbaneoHandler
 };
--- src/routes/NodeMailerRouter.js
+++ src/routes/NodeMailerRouter.js
@@ -7,12 +7,14 @@ const {
   getNodeMailerReservaHandler,
   getNodeMailerBaneoHandler,
   getNodeMailerCreacionDeHotelHandler,
+  getNodeMailerDesbaneoHandler
 } = require("../handlers/getNodeMailerHandler");
 
 NodeMailerRouter.get("/", getNodeMailerHandler);
 NodeMailerRouter.get("/Registro/:gmail", getRegistroNodeMailerHandler);
 NodeMailerRouter.get("/Reserva/:gmail", getNodeMailerReservaHandler);
 NodeMailerRouter.get("/Baneo/:gmail", getNodeMailerBaneoHandler);
+NodeMailerRouter.get("/Desbaneo/:gmail", getNodeMailerDesbaneoHandler);
 NodeMailerRouter.get(
   "/CreacionDeHotel/:gmail",
   getNodeMailerCreacionDeHotelHandler
